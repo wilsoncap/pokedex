5d1512a002293c92de461abc40ca53ebdf30d133
wilson capacho <wiston.98@hotmail.com>
2026-08-16 22:59:20 -0500

    create custom pipe for mongoid

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -10,6 +10,7 @@ import {
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { ParseMongoIdPipePipe } from '../common/pipes/parse-mongo-id-pipe/parse-mongo-id-pipe.pipe';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -39,7 +40,7 @@ export class PokemonController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseMongoIdPipePipe) id: string) {
     return this.pokemonService.remove(id);
   }
 }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -79,8 +79,10 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    /*const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();*/
+    const result = this.pokemonModel.findByIdAndDelete(id);
+    return result;
   }
 
   private handleExceptions(error: any) {
