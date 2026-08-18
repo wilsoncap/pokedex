ece4c89ec141e742f4afb3a67c8138ddcdd2cc2c
wilson capacho <wiston.98@hotmail.com>
2026-08-17 22:20:32 -0500

    tarea resuleta

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
     const { data } = await axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    for (const { name, url } of data.results) {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({name, no});
-    });
-    return data.results;
+      await this.pokemonModel.create({ no, name });
+
+      console.log({ name, no });
+    }
+    return 'seed executed';
   }
 }
